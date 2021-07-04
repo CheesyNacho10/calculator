4c2be3f9c80ae8c92c7e13901f9925e72beb142e
CheesyNacho10 <nachodl.queso@gmail.com>
2021-07-04 20:11:44 +0200

    Added buttons and started interaction

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let dispNumber = 0;
+let firts = 0;
+let operator = "";
+let result = true;
+
 const add = function(a, b) { return a + b; }
 const subtract = function(a, b) { return a - b; }
 const multiply = function(a, b) { return a * b; }
@@ -12,6 +17,46 @@ const operate = function(a, b, op) {
         case "*":
             return multiply(a, b);
         case "/":
+            if(b == 0) return "ERROR"
             return divide(a, b);
     }
+}
+
+function refresh() { document.getElementById("res").textContent = dispNumber; }
+
+function number(n) {
+    if(result) {
+        dispNumber = n;
+        result = false;
+    }
+    else {
+        dispNumber = dispNumber * 10 + n;
+    }
+    refresh();
+}
+
+function operation(s) {
+    firts = dispNumber;
+    operator = s;
+    dispNumber = 0;
+}
+
+function res() {
+    if(!result) {
+        dispNumber = operate(firts, dispNumber, operator);
+        result = true;
+    }
+    refresh();
+}
+
+function fullClear() {
+    dispNumber = 0;
+    firts = 0;
+    operator = "";
+    refresh();
+}
+
+function erase() {
+    dispNumber = (dispNumber - dispNumber % 10) / 10;
+    refresh();
 }
