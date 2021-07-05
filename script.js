e66468486f3260f81beaee24f54d3cdb0ee34cdf
CheesyNacho10 <nachodl.queso@gmail.com>
2021-07-05 21:04:56 +0200

    Fixed and caused other problems

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+/**
+ * Local variables
+ */
 let dispNumber = 0;
 let firts = 0;
+let second = 0;
 let operator = "";
-let result = true;
+let dot = 0;
 
+/**
+ * Primary functions
+ * @param {firts member} a 
+ * @param {second member} b 
+ * @returns Desired result
+ */
 const add = function(a, b) { return a + b; }
 const subtract = function(a, b) { return a - b; }
 const multiply = function(a, b) { return a * b; }
@@ -22,41 +32,96 @@ const operate = function(a, b, op) {
     }
 }
 
-function refresh() { document.getElementById("res").textContent = dispNumber; }
+/**
+ * Action functions
+ */
 
+/**
+ * Updates the displayed number and refresh
+ * @param {Number picked} n 
+ */
 function number(n) {
-    if(result) {
-        dispNumber = n;
-        result = false;
+    if(!dot) {
+        dispNumber = (dispNumber * 10) + n;
     }
     else {
-        dispNumber = dispNumber * 10 + n;
+        dispNumber = dispNumber + (n * Math.pow(10, - dot));
+        if(dot < 13) dot++;
     }
     refresh();
 }
 
+/**
+ * 
+ * @param {Operation picked} s 
+ */
 function operation(s) {
-    firts = dispNumber;
+    // Save the number
+    saveNumber();
+    // Set the operation
     operator = s;
-    dispNumber = 0;
+    // Concadenate operations
+    if(second) res();
+    dot = 0;
 }
 
+/**
+ * Makes the operation, displays it
+ * and resets the variables
+ */
 function res() {
-    if(!result) {
-        dispNumber = operate(firts, dispNumber, operator);
-        result = true;
-    }
+    if(!second) saveNumber();
+    dispNumber = operate(firts, second, operator);
     refresh();
+    firts = dispNumber;
+    dispNumber = 0;
+    second = 0;
+    dot = 0;
 }
 
+/**
+ * Clears all variables and resfreshes the display
+ */
 function fullClear() {
     dispNumber = 0;
     firts = 0;
+    second = 0;
     operator = "";
+    dot = 0;
     refresh();
 }
 
+/**
+ * Removes the las digit from the display and refreshes the display
+ */
 function erase() {
-    dispNumber = (dispNumber - dispNumber % 10) / 10;
-    refresh();
+    
+}
+
+/**
+ * Let the user to introduce decimals
+ */
+function decimal() {
+    if(dot == 0) dot = 1;
+}
+
+//Auxiliar functions
+
+/**
+ * Refresh the display with dispNumber
+ */
+function refresh() { document.getElementById("res").textContent = dispNumber.toFixed(dot ? dot - 1 : dot); }
+
+/**
+ * Resets variables for the new number
+ */
+function newNumber() {}
+
+/**
+ * Saves the number in the correct variable
+ * and resets the input variable
+ */
+function saveNumber() {
+    (!firts) ? firts = dispNumber : second = dispNumber;
+    dispNumber = 0;
 }
